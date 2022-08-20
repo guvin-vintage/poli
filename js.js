e5e9d3be8415adea251e627bb88569196e0bb714
guvin-vintage <109299594+guvin-vintage@users.noreply.github.com>
2022-08-21 00:40:16 +0700

    a

--- js.js
+++ js.js
@@ -1,3 +1,7 @@
+$(window).on('load', function (event) {
+    $('body').removeClass('preloading');
+    $('.load').delay(1000).fadeOut('fast');
+})
 $(document).ready(function() {
     
      /*SEARCH*/
